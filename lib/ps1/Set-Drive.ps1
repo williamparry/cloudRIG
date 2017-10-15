4553f55f0cff6e7a59861a799bf361934722684d
William Parry <williamparry@gmail.com>
2017-10-15 13:53:19 +0200

    Fixes

--- lib/ps1/Set-Drive.ps1
+++ lib/ps1/Set-Drive.ps1
@@ -1,5 +1,5 @@
 Get-Disk |
-Where-Object { $_.Size -eq "256GB" } |
+Where-Object { $_.Size -eq "100GB" } |
 Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -AssignDriveLetter -DriveLetter D -UseMaximumSize |
 Format-Volume -FileSystem NTFS -NewFileSystemLabel "Games" -Confirm:$false
