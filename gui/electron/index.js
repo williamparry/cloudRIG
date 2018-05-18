86a323f8745ac5de895f58f2f9b36592beca8e7f
William Parry <william.parry@techin.site>
2018-05-18 10:09:46 +1000

    Update build process

--- gui/electron/index.js
+++ gui/electron/index.js
@@ -1,4 +1,5 @@
-const common = require('./common')
+const common = require("./common");
+const path = require('path');
 
 // The profile names should match the ones in ~/.aws/credentials
 let testCredentialsFile = `
@@ -8,25 +9,28 @@ aws_secret_access_key=testaccesskeyid
 [cloudrig]
 aws_access_key_id=testaccessk2eyid3
 aws_secret_access_key=testacces2skeyid3
-`
+`;
 
-common.registerCMDHook("getCredentials", function(event, op, data) {
-	event.returnValue = testCredentialsFile;
-})
+common.registerCMDHook("getCredentials", function (event, op, data) {
+  event.returnValue = testCredentialsFile;
+});
 
-common.registerCMDHook("saveCredentialsFile", function(event, op, data) {
-	testCredentialsFile = data;
-	event.returnValue = true;
-})
+common.registerCMDHook("saveCredentialsFile", function (event, op, data) {
+  testCredentialsFile = data;
+  event.returnValue = true;
+});
 
-common.registerCMDHook("updateFail", function(event, op, data) {
-	event.sender.send('updateCheck', false)
-})
+common.registerCMDHook("updateFail", function (event, op, data) {
+  event.sender.send("updateCheck", false);
+});
 
-common.init({
-	pathname: 'localhost:3000',
-	protocol: 'http:',
-	slashes: true
-}, function(win) {
-	win.webContents.openDevTools()
-})
+common.init(
+  {
+    pathname: path.join(__dirname, 'dev.html'),
+    protocol: 'file:',
+    slashes: true
+  },
+  function (win) {
+
+  }
+);
