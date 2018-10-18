451cd73b27478b9eb17192db22007ae8113e2f13
Eugene-Bulog <eugene.bulog@gmail.com>
2018-10-19 11:53:07 +1300

    update createcloudwatchevent lambda with common

---
+++ lambdas/cloudrig-createCloudWatchEvent/index.js
@@ -0,0 +1,84 @@
+exports.handler = (event, context, callback) => {
+
+    var common = require("cloudrigLambdaCommon");
+    var AWS = require("aws-sdk");
+    var sns = new AWS.SNS();
+    var ec2 = new AWS.EC2();
+    var cloudwatchevents = new AWS.CloudWatchEvents();
+
+    var eventBody = JSON.parse(event.Records[0].Sns.Message);
+    var lambdaARNQueue = eventBody.lambdaARNQueue;
+
+    function run() {
+
+        var lambda = new AWS.Lambda();
+        
+        common.report("Creating CloudWatch rule");
+
+        cloudwatchevents.putRule(
+            {
+                Name: "cloudrig-watch-" + eventBody.state.instance.InstanceId,
+                EventPattern: JSON.stringify({
+                    source: ["aws.ec2"],
+                    "detail-type": ["EC2 Instance State-change Notification"],
+                    detail: {
+                        state: ["stopped"],
+                        "instance-id": [eventBody.state.instance.InstanceId]
+                    }
+                }),
+                State: "ENABLED"
+            },
+            function (err, ruleData) {
+                if (err) {
+                    common.report(err);
+                    return;
+                }
+
+                common.report("Creating CloudWatch target");
+
+                cloudwatchevents.putTargets(
+                    {
+                        Rule: "cloudrig-watch-" + eventBody.state.instance.InstanceId,
+                        Targets: [
+                            {
+                                Arn: eventBody.settings.lambda.FunctionArn,
+                                Id: "1"
+                            }
+                        ]
+                    },
+                    function (err) {
+                        if (err) {
+                            common.report(err);
+                            return;
+                        }
+
+                        common.report("Giving CloudWatch lambda permission");
+
+                        lambda.addPermission(
+                            {
+                                Action: "lambda:InvokeFunction",
+                                FunctionName: "cloudrig-lambda",
+                                Principal: "events.amazonaws.com",
+                                SourceArn: ruleData.RuleArn,
+                                StatementId: "Statement-" + eventBody.state.instance.InstanceId
+                            },
+                            function(err,data) {
+                                if (err) {
+                                    common.report(err);
+                                    return;
+                                }
+
+                                common.triggerNextLambda(lambdaARNQueue, eventBody);
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    }
+
+    common.start(run, eventBody);
+
+
+
+}
