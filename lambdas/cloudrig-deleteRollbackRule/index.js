e29264144e79362a2fbf1a4317d6826e0a32a4ee
Eugene-Bulog <eugene.bulog@gmail.com>
2018-10-19 12:05:22 +1300

    update deleteRollbackRule with common

---
+++ lambdas/cloudrig-deleteRollbackRule/index.js
@@ -0,0 +1,39 @@
+exports.handler = (event, context, callback) => {
+
+    var common = require("cloudrigLambdaCommon");
+    var AWS = require("aws-sdk");
+    var sns = new AWS.SNS();
+    var ec2 = new AWS.EC2();
+    var cloudwatchevents = new AWS.CloudWatchEvents();
+
+
+    var eventBody = JSON.parse(event.Records[0].Sns.Message);
+    var lambdaARNQueue = eventBody.lambdaARNQueue;
+
+
+    function run() {
+
+        common.report("Deleting startup interruption rule");
+        cloudwatchevents.removeTargets(
+            {
+                Rule: "cloudrig-startup-watch",
+                Ids: ["1"]
+            },
+            function (err, data) {
+                cloudwatchevents.deleteRule({
+                    Name: "cloudrig-startup-watch"
+                },
+                    function (err, data) {
+                        if (err) {
+                            common.report(err);
+                            return;
+                        }
+                        common.triggerNextLambda(lambdaARNQueue, eventBody);
+                    });
+            });
+    }
+
+
+    run();
+
+}
